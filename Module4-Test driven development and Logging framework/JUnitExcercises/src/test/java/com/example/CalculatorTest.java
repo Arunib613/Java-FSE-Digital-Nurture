f013213955f83d3ebea1af4be771855ea8dd9e05
Arunib613 <arunib613@gmail.com>
2026-06-28 23:44:15 +0530

    Add files via upload

---
+++ Module4-Test driven development and Logging framework/JUnitExcercises/src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,9 @@
+package com.example;
+import org.junit.Test;
+
+public class CalculatorTest {
+    @Test
+    public void testExample() {
+        System.out.println("JUnit is working successfully!");
+    }
+}
