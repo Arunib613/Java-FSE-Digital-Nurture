e1df858460df097663bf54574d39136f6a798d8a
Arunib613 <arunib613@gmail.com>
2026-07-01 22:35:40 +0530

    Add files via upload

---
+++ SpringCoreandMaven/Ex:04 CreatingandConfiguringMaven/src/test/java/Test.java
@@ -0,0 +1,7 @@
+import org.springframework.context.ApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Spring dependencies added successfully!");
+    }
+}
