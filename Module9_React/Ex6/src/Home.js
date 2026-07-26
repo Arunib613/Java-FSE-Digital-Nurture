cab5dfc13273c2c7613a5103ef3dc81cd21fbf83
Arunib613 <arunib613@gmail.com>
2026-07-26 15:44:36 +0530

    TrainersApp

---
+++ Module9_React/Ex6/src/Home.js
@@ -0,0 +1,17 @@
+function Home() {
+
+    return (
+
+        <div>
+
+            <h2>Welcome to Trainers App</h2>
+
+            <p>Select Trainers to view the trainer details.</p>
+
+        </div>
+
+    );
+
+}
+
+export default Home;
